2004fc9fa4e100e8c26f42de096be9e3d077e9f4
player-number-three <wenhao_92@yahoo.com.sg>
2022-08-04 17:41:18 +0800

    ver 2 added 2 player roll and choose dice

--- script.js
+++ script.js
@@ -7,17 +7,22 @@
 var diceRollGameMode = "DICE ROLL GAME MODE";
 var diceChooseMode = "DICE CHOOSE MODE";
 var gameMode = "DICE ROLL GAME MODE";
+var diceCompareSCore = "DICE ROLL COMPARE SCORES";
 // player rolls array
-playerRolls = [];
+var currentPlayerRolls = [];
+// current player
+var currentPlayer = 1;
+//player score
+var allPlayerScore = [];
 // player rolls function
 var rollDiceForPlayer = function () {
   var counter = 0;
   while (counter < 2) {
-    playerRolls.push(diceRoll());
-    console.log(playerRolls, `dice roll for player`);
+    currentPlayerRolls.push(diceRoll());
+    console.log(currentPlayerRolls, `dice roll for player`);
     counter = counter + 1;
   }
-  return `Hello Player ! Your dice rolls : <br> Dice 1 : ${playerRolls[0]} <br> Dice 2 : ${playerRolls[1]}`;
+  return `Hello Player ${currentPlayer} ! Your dice rolls : <br> Dice 1 : ${currentPlayerRolls[0]} <br> Dice 2 : ${currentPlayerRolls[1]}`;
 };
 // dice roll function
 var diceRoll = function () {
@@ -31,70 +36,96 @@ var diceRoll = function () {
 // player score function
 
 var getPlayerScore = function (playerInput) {
+  var playerScore;
   // if input is not 1 or 2
   if (playerInput != 1 && playerInput != 2) {
     console.log(`if player fails to input 1 or 2`);
-    myOutputValue =
-      "Please key in either 1 or 2 to choose the Dice that you want to be the first number.";
+    return `Please key in either 1 or 2 to choose the Dice that you want to be the first number <br> Dice 1 : ${currentPlayerRolls[0]} <br> Dice 2 : ${currentPlayerRolls[1]}`;
   }
+
   // if input is 1
   if (playerInput == 1) {
     console.log(`if player manages to input 1`);
-    var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
-
-    myOutputValue = `Hello Player,  your score is ${playerScore}`;
+    var playerScore = Number(
+      String(currentPlayerRolls[0]) + String(currentPlayerRolls[1])
+    );
+    // myOutputValue = `Hello Player ${currentPlayer},  your score is ${playerScore}`;
   }
 
   // if input is 2
   if (playerInput == 2) {
     console.log(`if player manages to input 2`);
-    var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
-    myOutputValue = `Hello Player,  your score is ${playerScore}`;
+    var playerScore = Number(
+      String(currentPlayerRolls[1]) + String(currentPlayerRolls[0])
+    );
+    // myOutputValue = `Hello Player ${currentPlayer},  your score is ${playerScore}`;
   }
-  return myOutputValue;
+
+  // store the player score
+  allPlayerScore.push(playerScore);
+  console.log("score has been pushed to all player score");
+
+  // clear the store value
+  currentPlayerRolls = [];
+  console.log(`current player rolls reset`);
+
+  return "your chosen value is " + playerScore;
+
+  // return `Hello player ${currentPlayer}! you have chosen ${playerScore}`;
 };
 
 var main = function (input) {
   console.log(gameMode, `current game mode`);
+  console.log(currentPlayer, ` current player `);
   var myOutputValue = "";
 
   if (gameMode == diceRollGameMode) {
     console.log(`game mode == diceRollGameMode`);
 
-    myOutputValue = `${rollDiceForPlayer()} <br> Please key in 1 or 2 to choose the Dice that you want to be the first number`;
+    myOutputValue = `${rollDiceForPlayer()} <br> Please key in 1 or 2 to choose the Dice that you want to be the first number `;
 
     gameMode = diceChooseMode;
-    console.log(`game mode changes to ${diceChooseMode}`);
+    console.log(`game mode changes to dice choose mode`);
 
     return myOutputValue;
   }
 
   if (gameMode == diceChooseMode) {
+    console.log(`player score input`);
     myOutputValue = getPlayerScore(input);
-    return myOutputValue;
+
+    if (currentPlayer == 1) {
+      console.log(` end of player 1 turn, beginning of player 2 turn`);
+      currentPlayer = 2;
+      gameMode = diceRollGameMode;
+      return myOutputValue + `<br> it is now player 2's turn <br>`;
+    }
+
+    if (currentPlayer == 2) {
+      console.log(`end of player 2 turn`);
+      gameMode = diceCompareSCore;
+      console.log(`gamemode changes to dice compare score`);
+    }
+    return `${myOutputValue} <br> Press submit to see the scores!`;
   }
 
-  //   // if input is not 1 or 2
-  //   if (input != 1 && input != 2) {
-  //     console.log(`if player fails to input 1 or 2`);
-  //     myOutputValue =
-  //       "Please key in either 1 or 2 to choose the Dice that you want to be the first number.";
-  //   }
-  //   // if input is 1
-  //   if (input == 1) {
-  //     console.log(`if player manages to input 1`);
-  //     var playerScore = Number(String(playerRolls[0]) + String(playerRolls[1]));
+  if (gameMode == diceCompareSCore) {
+    // if tie
+    if (allPlayerScore[0] == allPlayerScore[1]) {
+      console.log(`its a tie`);
+      myOutputValue = " It is a TIE! No one wins!";
+    }
 
-  //     myOutputValue = `Hello Player,  your score is ${playerScore}`;
-  //   }
-
-  //   // if input is 2
-  //   if (input == 2) {
-  //     console.log(`if player manages to input 2`);
-  //     var playerScore = Number(String(playerRolls[1]) + String(playerRolls[0]));
-  //     myOutputValue = `Hello Player,  your score is ${playerScore}`;
-  //   }
-
-  //   return myOutputValue;
-  // }
+    //if player 1 win
+    if (allPlayerScore[0] > allPlayerScore[1]) {
+      console.log("player 1 wins!");
+      myOutputValue = "Player 1 WINS!";
+    }
+    //if player 2 win
+    if (allPlayerScore[0] < allPlayerScore[1]) {
+      console.log("player 2 wins!");
+      myOutputValue = "Player 2 WINS!";
+    }
+    return myOutputValue + `<br> Click Submit to play again!`;
+  }
 };
